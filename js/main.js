dde695f4d73258a523d7141d265d1083cb0f3d1f
Omar Ragab <158287908+Omar-Ragab-Projects@users.noreply.github.com>
2024-07-21 23:48:53 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,9 +4,19 @@ let userIP = document.querySelector("span.user-ip");
 let userLocation = document.querySelector("span.user-location");
 let userTimezone = document.querySelector("span.user-timezone");
 let userISP = document.querySelector("span.user-isp");
+
+// See Own IP Information on the initial page load
+fetchGetData();
+
 // Getting User Data After Submit
 submitBtn.addEventListener("click", () => {
+  fetchGetData();
+});
+
+// Getting User Data
+function fetchGetData() {
   let url = `https://geo.ipify.org/api/v1?apiKey=at_2RbplB5OgzZO2u0ZdDxJQGm0a70L8&ipAddress=${inputText.value}`;
+
   fetch(url)
     .then((resolvedValue) => {
       return resolvedValue.json();
@@ -21,7 +31,8 @@ submitBtn.addEventListener("click", () => {
       map.setView([data.location.lat, data.location.lng], 13);
       let marker = L.marker([data.location.lat, data.location.lng]).addTo(map);
     });
-});
+}
+
 // Make The Map
 let map = L.map("map").setView([51.505, -0.09], 13);
 L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
